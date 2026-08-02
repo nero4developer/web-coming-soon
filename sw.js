2fdb69253315ecc258995b83347324322a6670ae
mendistuts <mendisworkmail@gmail.com>
2026-08-03 03:29:23 +0530

    update UI/UX fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'nerovoxx-v2';
-const ASSETS = ['/', '/index.html', '/icon.svg', '/manifest.json'];
+const CACHE = 'nerovoxx-v3';
+const ASSETS = ['/', '/index.html', '/icon.svg', '/icon-192.png', '/icon-512.png', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
